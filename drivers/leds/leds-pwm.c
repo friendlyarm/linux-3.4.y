e5d3580924b8e73629ceaccb40c9d5e1d1d03b63
jensen <jensenhuang@friendlyarm.com>
2016-02-18 10:52:26 +0800

    leds: leds-pwm: implement PWM inversion

--- drivers/leds/leds-pwm.c
+++ drivers/leds/leds-pwm.c
@@ -37,14 +37,16 @@ static void led_pwm_set(struct led_classdev *led_cdev,
 		container_of(led_cdev, struct led_pwm_data, cdev);
 	unsigned int max = led_dat->cdev.max_brightness;
 	unsigned int period =  led_dat->period;
+	unsigned int duty = brightness * period / max;
 
-	if (brightness == 0) {
-		pwm_config(led_dat->pwm, 0, period);
+	if (led_dat->active_low)
+		duty = period - duty;
+
+	pwm_config(led_dat->pwm, duty, period);
+	if (brightness == 0)
 		pwm_disable(led_dat->pwm);
-	} else {
-		pwm_config(led_dat->pwm, brightness * period / max, period);
+	else
 		pwm_enable(led_dat->pwm);
-	}
 }
 
 static int led_pwm_probe(struct platform_device *pdev)
